5a66eaa6228c125fef3febfdb774ed7c7c8c6828
TakayukiTooyama <t.tooyama107@gmail.com>
2022-02-02 16:07:27 +0900

    初期設定

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Front学習</title>
+        <meta name="description" content="FrontEndの学習手引き" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
